fa96930d02b1b92fa457d85ccc760237fe8bacc1
wigarbadri <wigarbadri@gmail.com>
2023-07-26 16:32:20 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+// deliverable 1
+const num1 = 31
+const num2 = 2
 
-// Write your code here
+// deliverable 2
+const multiply = num1 * num2
+
+const random = Math.floor(Math.random() * 100000000) + 1
+
+//deliverable 3
+const num3 = 44
+const num4 = 8
+const mod = num3 % num4
+
+// deliverable 4
+const max = Math.max (5, 10, 15, 20)
